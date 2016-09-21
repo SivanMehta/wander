4c3da12232c8eaa4dae5c791439e6fcf1949baf6
gauryn <gauryn22@gmail.com>
2016-09-20 20:53:22 -0400

    Removed google maps geocoder from app.js

--- src/app.js
+++ src/app.js
@@ -2,36 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 import Users from './users'
 
-function reverseGeocode(lat, lng){
-  var latlng = {lat: lat, lng: lng};
-  // Geocoder to get address from lat/lng
-  var geocoder = new google.maps.Geocoder;
-  geocodeLatLng(geocoder, latlng)
-}
-
-function geocodeLatLng(geocoder, latlng) {
-    geocoder.geocode({'location': latlng}, function(results, status)
-    {
-      if (status === 'OK') 
-      {
-        if (results[1]) 
-        {
-          // console log formatted address
-          console.log("Address: ", results[1].formatted_address)
-          return results[1].formatted_address;
-        } 
-        else 
-        {
-          window.alert('No results found');
-        }
-      } 
-      else 
-      {
-        window.alert('Geocoder failed due to: ' + status);
-      }
-    });
-}
-
 export default class App extends React.Component {
   constructor(props) {
     super(props)
