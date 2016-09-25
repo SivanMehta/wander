9595170bf22037a92c26a7800154ee719abb0721
Yeon Soo Kim <yeonsook@YeonSooKimsMBP.wv.cc.cmu.edu>
2016-09-25 16:14:20 -0400

    Updated nav bar changes

--- src/app.js
+++ src/app.js
@@ -27,21 +27,10 @@ export default class App extends React.Component {
 
   }
 
-  homepage(event) {
-    this.setState({page: 'users'}) // Trying to link back to page of users
-  }
-
-  render() {
+  renderContent() {
     if(this.state.page == 'login') {
       return(
-          <div className = 'container'>
-            <div className="navbar navbar-default">
-              <div className="container-fluid">
-                <div className="navbar-header">
-                  <p id="logo">Wander</p>
-                </div>
-              </div>
-            </div>
+          <div>
             <h2 id="title">Welcome to Wander! </h2>
             <div className = 'col-sm-12 col-md-6'>
               <img src="img/image.png" id="home_img"></img>
@@ -55,18 +44,37 @@ export default class App extends React.Component {
                     <input type="text" id = "pw_input" className="form-control" placeholder="Password"></input>
                 </div>
               </form>
-                <div className = 'col-sm-12 col-md-6'>
-                  <button type="button btn-block" className = "btn btn-secondary" id = "tbutton" onClick = { this.connect }>Log-in as Tourist</button>
-                </div>
-                <div className = 'col-sm-12 col-md-6'>
-                  <button type="button btn-block" className = "btn btn-secondary" id = "gbutton" onClick = { this.connect }>Log-in as Guide</button>
-                </div>
+              <div className = 'col-sm-12 col-md-6'>
+                <button type="button btn-block" className = "btn btn-secondary" id = "tbutton" onClick = { this.connect }>Log-in as Tourist</button>
+              </div>
+              <div className = 'col-sm-12 col-md-6'>
+                <button type="button btn-block" className = "btn btn-secondary" id = "gbutton" onClick = { this.connect }>Log-in as Guide</button>
+              </div>
             </div>
           </div>
         )
     } else if (this.state.page == 'loading') {
       return(
-        <div className = 'container'>
+        <center>
+          <i className="fa fa-repeat" aria-hidden="true"></i>
+        </center>
+      )
+    } else {
+      return(
+        <Users role = { this.state.role }
+                 view = { 'users' }
+                 position = { this.state.position }/>
+      )
+    }
+  }
+
+  homepage(event) {
+    this.setState({page: 'users'}) // Trying to link back to page of users
+  }
+
+  render() {
+    return(
+        <div>
           <div className="navbar navbar-default">
             <div className="container-fluid">
               <div className="navbar-header">
@@ -74,34 +82,11 @@ export default class App extends React.Component {
               </div>
             </div>
           </div>
-          <center>
-            <i className="fa fa-repeat" aria-hidden="true"></i>
-          </center>
-        </div>
-      )
-    } else {
-      return (
-        <div className = 'container'>
-          <div className="navbar navbar-default">
-            <div className="container-fluid">
-              <div className="navbar-header">
-                <div className = 'col-sm-4'>
-                  <button type="button" className = "btn btn-secondary btn-large" id="home_btn" onClick = { this.homepage }>
-                    <i className="fa fa-home" aria-hidden="true"></i>
-                  </button>
-                </div>
-                <div className = 'col-sm-4'>
-                  <p id="logo">Wander</p>
-                </div>
-              </div>
-            </div>
+          <div className = 'container'>
+            { this.renderContent() }
           </div>
-          <Users role = { this.state.role }
-                 view = { 'users' }
-                 position = { this.state.position }/>
         </div>
       )
-    }
   }
 }
 
